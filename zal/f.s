dfefa65994ee2c0f25014215df9c90fec7bb6f44
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-05-28 17:58:39 +0200

    optimized

--- zal/f.s
+++ zal/f.s
@@ -17,45 +17,50 @@ load:
 	test bl, bl
 	jz end
 
-first:
-	cmp bl, 'a'
-	jl second
-	sub bl, 0x20
+	and ebx, 0xFFFFFFDF
+	or ebx, 0x00002000
+	xor ebx, 0x20200000
 
-second:
-	cmp bh, 'Z'
-	jg check_last_two
-	add bh, 0x20
-
-check_last_two:
-	mov ecx, ebx
-	shr ecx, 16
-	mov esi, 0x2000
-
-third:
-	cmp ch, 'Z'
-	jl modify_third
-	mov esi, -0x2000
-
-modify_third:
-	add ecx, esi
-	mov esi, 0x20
-
-fourth:
-	cmp cl, 'Z'
-	jl modify_fourth
-	mov esi, -0x20
-
-modify_fourth:
-	add ecx, esi
-
-save:
-	shl ecx, 16
-	movsx ebx, bx
-	add ecx, ebx
-	mov [eax], ecx
+	mov [eax], ebx
 	lea eax, [eax + 4]
 	jmp load
+
+; first:
+; 	cmp bl, 'a'
+; 	jl second
+; 	sub bl, 0x20
+
+; second:
+; 	cmp bh, 'Z'
+; 	jg check_last_two
+; 	add bh, 0x20
+
+; check_last_two:
+; 	mov ecx, ebx
+; 	shr ecx, 16
+; 	mov esi, 0x2000
+
+; third:
+; 	cmp ch, 'Z'
+; 	jl modify_third
+; 	mov esi, -0x2000
+
+; modify_third:
+; 	add ecx, esi
+; 	mov esi, 0x20
+
+; fourth:
+; 	cmp cl, 'Z'
+; 	jl modify_fourth
+; 	mov esi, -0x20
+
+; modify_fourth:
+; 	add ecx, esi
+
+; save:
+; 	mov [eax], ebx
+; 	lea eax, [eax + 4]
+; 	jmp load
 
 end:
 	pop edx
